6b03bdf887776fc27ad1726342e0a21f6086e292
Dale Talley III <dtalley32@icloud.com>
2023-09-13 16:44:42 -0500

    fix html. setup js var

---
+++ script.js
@@ -0,0 +1,23 @@
+//Declare a variable to store the searched city
+var city="";
+// variable declaration
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var sCity=[];
+// searches the city to see if it exists in the entries from the storage
+function find(c){
+    for (var i=0; i<sCity.length; i++){
+        if(c.toUpperCase()===sCity[i]){
+            return -1;
+        }
+    }
+    return 1;
+}
+//Set up the API key
+var APIKey="3d3b0e4d5f9bf1670eb5f579eaf977bf";
