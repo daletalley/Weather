34620b33d2a5e4817af0a0691691b5fa49618d8f
Dale Talley III <dtalley32@icloud.com>
2023-09-13 16:52:18 -0500

    APIKey set up and functions for storage and search

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Declare a variable to store the searched city
 var city="";
-// variable declaration
+// Variable declaration
 var searchCity = $("#search-city");
 var searchButton = $("#search-button");
 var clearButton = $("#clear-history");
@@ -10,7 +10,7 @@ var currentHumidty= $("#humidity");
 var currentWSpeed=$("#wind-speed");
 var currentUvindex= $("#uv-index");
 var sCity=[];
-// searches the city to see if it exists in the entries from the storage
+// Searches the city to see if it exists in the storage
 function find(c){
     for (var i=0; i<sCity.length; i++){
         if(c.toUpperCase()===sCity[i]){
@@ -19,5 +19,13 @@ function find(c){
     }
     return 1;
 }
-//Set up the API key
+//API key
 var APIKey="3d3b0e4d5f9bf1670eb5f579eaf977bf";
+// Display current & future weather to the user after getting the city form the input.
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
